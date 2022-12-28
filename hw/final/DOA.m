2a42e6791b012e894a68be740bf85a130b68459e
ray-qwer <b07901055@ntu.edu.tw>
2022-12-28 17:48:24 +0800

    1228

--- hw/final/DOA.m
+++ hw/final/DOA.m
@@ -20,65 +20,69 @@ plot(t,theta_i_noisy);
 %     theta_s_hat(i) = x*conj(w);
 % end
 
-figure(2);
-fft_s = fft(theta_s_noisy);
-fft_s( abs(fft_s) < max(abs(fft_s))/70)=0;
-% fft_s = fftshift(fft_s);
-% fft_s = fftshift(fft_s);
-subplot(2,1,1);
-plot(1:length(fft_s), fft_s);
 
-theta_s_ifft = ifft(fft_s);
-subplot(2,1,2);
-plot(t, theta_s_ifft);
-hold on;
-% subplot(3,1,3);
-plot(t, theta_s_noisy);
-hold off;
-
+IMF_set_i = hht(theta_i_noisy, t, 4);
+figure(2)
+for i = 1:size(IMF_set_i,1)
+    subplot(size(IMF_set_i,1),1,i);
+    plot(t, IMF_set_i(i,:));
+end
 figure(3);
-fft_i = fft(theta_i_noisy);
-fft_i( abs(fft_i) < max(abs(fft_i))/70)=0;
-% fft_s = fftshift(fft_s);
-% fft_s = fftshift(fft_s);
-subplot(3,1,1);
-plot(1:length(fft_i), fft_i);
-
-theta_i_ifft = ifft(fft_i);
-subplot(3,1,2);
-plot(t, theta_i_ifft);
-hold on;
+recon_i = sum(IMF_set_i(2:end,:),1);
 plot(t, theta_i_noisy);
+hold on;
+plot(t,recon_i);
 hold off;
 
-figure(4);
-[~,w]=ASP_RLS(theta_i_noisy, theta_i_ifft,0.01, 0.9,10);
+IMF_set_s = hht(theta_s_noisy, t, 3);
+figure(4)
+for i = 1:size(IMF_set_s,1)
+    subplot(size(IMF_set_s,1),1,i);
+    plot(t, IMF_set_s(i,:));
+end
+figure(5);
+recon_s = sum(IMF_set_s(3:end,:),1);
+plot(t, theta_s_noisy);
+hold on;
+plot(t,recon_s);
+hold off;
+
+figure(6);
+plot(t,recon_i);
+hold on;
+plot(t,recon_s);
+hold off;
+
+figure(7);
+[~,w]=ASP_RLS(theta_i_noisy, recon_i,0.01, 0.9,10);
 theta_i_hat = zeros(size(theta_i_noisy));
 tmp = [ones(1,9)*theta_i_noisy(1) theta_i_noisy];
 for i = 1:length(theta_i_hat)
     theta_i_hat(i) = w(:,i)'* flip(tmp(i:i+9)).';
 end
-subplot(211);
-plot(t, theta_i_hat);
-hold on;
+
 plot(t, theta_i_noisy);
-plot(t, theta_i_ifft);
-hold off;
-subplot(212);
-plot(t,theta_i_hat);
 hold on;
-plot(t, theta_i_ifft);
+plot(t, theta_i_hat);
 hold off;
 
-IMF_set = hht(theta_i_noisy, t, 4);
-figure(5)
-for i = 1:size(IMF_set,1)
-    subplot(size(IMF_set,1),1,i);
-    plot(t, IMF_set(i,:));
+figure(8);
+[~,w]=ASP_RLS(theta_s_noisy, recon_s,0.01, 0.9,10);
+theta_s_hat = zeros(size(theta_s_noisy));
+tmp = [ones(1,9)*theta_s_noisy(1) theta_s_noisy];
+for i = 1:length(theta_s_hat)
+    theta_s_hat(i) = w(:,i)'* flip(tmp(i:i+9)).';
 end
-figure(6);
-recon_s = sum(IMF_set(2:end,:),1);
-plot(t, theta_i_noisy);
+plot(t, theta_s_noisy);
 hold on;
-plot(t,recon_s);
+plot(t, theta_s_hat);
 hold off;
+
+figure(9);
+plot(t, theta_s_hat);
+hold on;
+plot(t, recon_s);
+plot(t, recon_i);
+plot(t, theta_i_hat);
+hold off;
+legend("theta_s_hat","recon_s","recon_i","theta_i_hat");
--- hw/final/beaformer.m
+++ hw/final/beaformer.m
@@ -5,17 +5,31 @@ theta_i_noisy = load("ASP_Final_Data.mat","theta_i_noisy");
 matX = matX.matX;
 theta_i_noisy = theta_i_noisy.theta_i_noisy;
 theta_s_noisy = theta_s_noisy.theta_s_noisy;
+t = [1:length(theta_s_noisy)];
 
-fft_s = fft(theta_s_noisy);
-fft_s( abs(fft_s) < max(abs(fft_s))/70)=0;
-theta_s_denoise = ifft(fft_s);
-fft_i = fft(theta_i_noisy);
-fft_i( abs(fft_i) < max(abs(fft_i))/70)=0;
-theta_i_denoise = ifft(fft_i);
+threshold = 0.006;
+IMF_set_i = hht(theta_i_noisy, t, 4);
+mean_set_i = mean(IMF_set_i,2);
+start_index_i = find(abs(mean_set_i) > threshold,1);
+theta_i_denoise = sum(IMF_set_i(start_index_i:end,:),1);
 
-g = [1 1E-5].';
+IMF_set_s = hht(theta_s_noisy, t, 3);
+mean_set_s = mean(IMF_set_s,2);
+start_index_s = find(abs(mean_set_s) > threshold,1);
+theta_s_denoise = sum(IMF_set_s(start_index_s:end,:),1);
+figure(1);
+plot(t,theta_s_denoise);
+hold on;
+plot(t,theta_i_denoise);
+hold off;
+title("denoised DOA");
+legend('$$\hat{\theta _s}(t)$$','$$\hat{\theta _i}(t)$$','Interpreter','latex');
+xlabel("time");
+ylabel("angle(degree)");
 
-delta = 0.01; lambda = 0.999;
+g = [1 0.002].';
+
+delta = 0.01; lambda = 0.995;
 w = zeros(size(matX));
 P = 1/delta*eye(size(matX,1));
 for i = 1:length(theta_s_noisy)
@@ -30,9 +44,17 @@ y = zeros(size(theta_s_noisy));
 for i = 1:length(theta_s_noisy)
     y(i) = w(:,i)' * matX(:,i);
 end
-t = [1:length(theta_s_noisy)];
+
 figure(2);
+% title("estimated source signal");
 subplot(211);
 plot(t, real(y));
+title('real part of $$\hat{s}(t)$$','Interpreter','latex');
+xlabel("time");
+ylabel("magnitude");
 subplot(212);
 plot(t, imag(y));
+title('imag part of $$\hat{s}(t)$$','Interpreter','latex');
+xlabel("time");
+ylabel("magnitude");
+sgtitle('estimated source signal $$\hat{s}(t)$$','Interpreter','Latex');
